7b6a25bf118977893d6795701268b0a009d7a19f
fatoudione97 <105082997+fatoudione97@users.noreply.github.com>
2022-05-14 14:32:52 +0000

    Add files via upload

---
+++ poo_mvcExamen/core/DataBase.php
@@ -0,0 +1,40 @@
+<?php 
+namespace App\Core;
+use App\Exceptions\BdConnexionException;
+class DataBase{
+    private \PDO|null $pdo=null;
+    public function openConnexion(){
+        try {
+            $this->pdo=new \PDO("mysql:dbname=gestion_scolaire_l2;host=localhost","root");
+        } catch (\Exception $ex) {
+              die("Erreur Connexion -Veuillez contacter votre Admin");
+        }
+       
+    }
+    public function closeConnexion(){
+         $this->pdo=null;
+    }
+
+    public function executeSelect(string $sql,array $data=[],$single=false){
+        $this->openConnexion();
+         $stm=$this->pdo->prepare($sql);
+         $stm->execute($data);
+        if($single){
+            $result=$stm->fetch();
+        }else{
+            $result=$stm->fetchAll();
+        }
+        $this->closeConnexion();
+        return  $result;
+        
+    }
+
+    public function executeUpdate(string $sql,array $data=[]):int{
+        $this->openConnexion();
+         $stm=$this->pdo->prepare($sql);
+         $stm->execute($data);
+         $result=$stm->rowCount();
+        $this->closeConnexion();
+        return  $result;
+    }
+}
---
+++ poo_mvcExamen/core/IModel.php
@@ -0,0 +1,10 @@
+<?php 
+namespace App\Core;
+interface IModel{
+    public function insert();
+    public function update();
+    public static function delete(int $id);
+    public static  function selectAll();
+    public static  function selectById(int $id);
+    public static  function selectWhere(string $sql,array $data=[],$single=false);
+}
